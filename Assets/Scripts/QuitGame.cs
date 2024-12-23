f6006ac4f93c0aca10777ddccf99b15cb30e7660
raminres <ramin.rasulzade@gmail.com>
2024-12-23 19:41:55 +0300

    Add hover and click sounds to buttons, add linkedin and github links, add credits and options menu anims

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -2,11 +2,32 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class QuitGame : MonoBehaviour
 {
+    
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip hoverSound;
+    public AudioClip clickSound;
     public void Quit()
     {
         Debug.Log("Game is quitting...");
+        PlayClickSound();
 
         SceneManager.LoadScene("LV_MainMenu");
         
+    }
+    public void PlayHoverSound()
+    {
+        if (hoverSound != null)
+        {
+            audioSource.PlayOneShot(hoverSound);
+        }
+    }
+
+    public void PlayClickSound()
+    {
+        if (clickSound != null)
+        {
+            audioSource.PlayOneShot(clickSound);
+        }
     }
 }
